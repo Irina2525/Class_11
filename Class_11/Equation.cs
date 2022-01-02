6e6cf47e401bea3bdae8b6c948fb316cd7e3065a
Irina <spartak.irina92@gmail.com>
2022-01-02 16:25:12 +0700

    Добавьте файлы проекта.

---
+++ Class_11/Equation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_11
+{
+    struct Equation
+    {
+        double k;    // закрытые поля структуры Equation (т.е. не public)
+        double b;
+
+        public Equation(double k, double b) // конструтор, чтобы устанавливать значения
+        {
+            this.k = k; //(обращаемя к полю)
+            this.b = b;
+        }
+
+        public string Root() //метод для решения уравнения (по заданию Root)
+                             // метод не принимает аргументы, возьмет их из структуры, т.к. имеет к ним доступ
+                             // метод вернет либо фразу либо число, поэтому string (выходное значение универсально строкой)
+        {
+            double x = b / k;
+            if (k == 0)
+                return "Нет решения, на 0 делить нельзя!";
+            return $"Решение:x={x:0.000}";
+        }
+
+    }
+
+
+}
---
+++ Class_11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //с клавиатуры добавляем два числа
+            Console.WriteLine("***Решения линейного уравнения 0=kx+b***");
+            Console.Write("ВВедите первый коэффициент уравнения b=");
+            double b = Convert.ToDouble(Console.ReadLine());  
+            Console.Write("ВВедите второй коэффициент уравнения k=");
+            double k = Convert.ToDouble(Console.ReadLine());
+
+            // теперь используем структуру, т.е. создаем экземпляр структуры
+            Equation equation = new Equation(k, b);
+
+            // вызываем метод Root (он вернет строку, мы ее выведем)
+            Console.WriteLine(equation.Root());
+            Console.ReadKey();
+        }
+    }
+}
